cc7ab42cff6119c24117dd8543d1d83428d110e7
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-07-01 12:37:09 -0400

    commit tree 713a8eaa04c4d812a0da210f422e42ddc8a82539

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
-  # write code here
+
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind
+    is_triangle?
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 != side2 && side2 != side3 && side3 != side1
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  def check_sides_for_zero
+    [side1, side2, side3].each do |side|
+      side.positive?
+    end
+  end
+
+  def is_valid_triangle?
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  end
+
+  def is_triangle?
+    raise TriangleError unless check_sides_for_zero && is_valid_triangle?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
+
+#if side1 + side2 > side3 || side1 + side3 > side2 || side2 + side3 > side1
+ # raise TriangleError
+#elsif side1 < 0 || side2 < 0 || side3 < 0
+#raise TriangleError
+#else
